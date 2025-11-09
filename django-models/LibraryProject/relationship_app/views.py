0899c17778941827fedcbf1c9039119482ddc0e6
omush-droid <omushephraim@gmail.com>
2025-11-09 17:19:02 +0300

    Add files via upload

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from .models import Book, Library
+from django.contrib.auth.decorators import user_passes_test
+from .models import Library
+from .models import Library, Book
 
 # Function-based view to list all books
 def list_books(request):
@@ -26,3 +28,35 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+# Role check functions
+def is_admin(user):
+    try:
+        return user.userprofile.role == 'Admin'
+    except:
+        return False
+
+def is_librarian(user):
+    try:
+        return user.userprofile.role == 'Librarian'
+    except:
+        return False
+
+def is_member(user):
+    try:
+        return user.userprofile.role == 'Member'
+    except:
+        return False
+
+# Role-based views
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')

418939c1dd7ea3b55736d117c1209ce716f8bdcb
omush-droid <omushephraim@gmail.com>
2025-11-22 18:29:36 +0300

    Add files via upload

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
-from rest_framework import generics
+import rest_framework.generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(generics.ListCreateAPIView):
+class BookList(rest_framework.generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer

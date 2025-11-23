22ef31185c092f5dcf216a8f5e87a8ffe6148218
omush-droid <omushephraim@gmail.com>
2025-11-23 14:00:25 +0300

    Add files via upload

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,12 +1,15 @@
 import rest_framework.generics
 import rest_framework.viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(rest_framework.generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookViewSet(rest_framework.viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]

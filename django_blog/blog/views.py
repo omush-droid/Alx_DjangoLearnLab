8756f932264b847e10b9de0b6878b8f7d89c7387
omush-droid <omushephraim@gmail.com>
2025-12-06 18:44:36 +0300

    Add files via upload

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -88,7 +88,7 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     
     def form_valid(self, form):
         form.instance.author = self.request.user
-        form.instance.post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        form.instance.post = get_object_or_404(Post, pk=self.kwargs['pk'])
         return super().form_valid(form)
     
     def get_success_url(self):

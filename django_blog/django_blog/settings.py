b958d3ea284b6c1fa701b0cdc9215c23d90c82e2
omush-droid <omushephraim@gmail.com>
2025-12-06 18:11:11 +0300

    Add files via upload

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -128,3 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
+
+# Authentication settings
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/login/'

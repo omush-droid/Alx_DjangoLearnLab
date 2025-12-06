5b977efd4fe9805809ff9a539f7958d569995ed9
omush-droid <omushephraim@gmail.com>
2025-12-06 19:00:06 +0300

    Add files via upload

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from taggit.forms import TagWidget
 from .models import Post, Comment
 
 class CustomUserCreationForm(UserCreationForm):
@@ -27,7 +28,7 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'content', 'tags')
         widgets = {
-            'tags': forms.TextInput(attrs={'placeholder': 'Enter tags separated by commas'})
+            'tags': TagWidget()
         }
 
 class CommentForm(forms.ModelForm):
